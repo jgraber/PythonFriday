22405b417492e6b69d81624878f2b95684ad66c1
Johnny Graber <jg@jgraber.ch>
2021-04-17 22:08:57 +0200

    add basic example for doctest

---
+++ doctest/doctestexample.py
@@ -0,0 +1,19 @@
+def is_even(num):
+    """
+    The is_even function returns True when the number is even:
+    
+    >>> is_even(4)
+    True
+    
+    The function returns false when the number is odd:
+    
+    >>> is_even(5)
+    False
+    
+    """
+    return num % 2 != 0
+
+# def test_4_is_even():
+#     assert True == is_even(4)
+    
+    
