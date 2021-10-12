40d1dba7cdb0cecab2d74a61506f1465c4eb7e70
Johnny Graber <jg@jgraber.ch>
2021-10-12 21:30:42 +0200

    add first example for pydriller

---
+++ helper/pydriller_examples.py
@@ -0,0 +1,27 @@
+from pydriller import Repository
+
+# for commit in Repository('https://github.com/jgraber/PythonFriday').traverse_commits():
+for commit in Repository('D:\Python\PythonFriday').traverse_commits():
+    print(f"{commit.hash} - {commit.committer_date} - {commit.author.name} - {commit.msg}")
+    for file in commit.modified_files:
+        print(f'  - {file.filename} has changed ({file.change_type.name})')
+        
+    print("")
+    
+# 18236534aa9c43de3e703c8a6a23395812b6d72a - 2021-06-14 00:04:26+02:00 - Johnny Graber - add constraints, indexes and default values
+#   - 5867629bf749_add_created_date_with_default_value_to_.py has changed (ADD)
+#   - cd496010379c_remove_constraints_for_examples.py has changed (ADD)
+#   - dff2171f8b62_create_index_for_employee_last_name_and_.py has changed (ADD)
+#   - e4887a8bedc7_add_index_to_publisher_name.py has changed (ADD)
+#   - book.py has changed (MODIFY)
+#   - employee.py has changed (MODIFY)
+#   - publisher.py has changed (MODIFY)
+
+# 3adbf91ab605a4ecf5b1bcc3fe1a4bccdc2729b2 - 2021-06-17 19:57:27+02:00 - Johnny Graber - add example for automap and SQL Server
+#   - sqlalchemy_orm_automap_sqlserver.py has changed (ADD)
+
+# e179d7750cd703b60a6ea0cdb03a174bd823e104 - 2021-10-06 21:49:39+02:00 - Johnny Graber - add example for PrettyPrinter
+#   - prettyprinter_example.py has changed (ADD)
+
+print("=========================================\n\n\n\n\n")
+
