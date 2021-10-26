eb6ff87be883150590d22fe4bd82d448f72d68bb
Johnny Graber <jg@jgraber.ch>
2021-10-26 20:55:00 +0200

    add more examples

--- helper/pydriller_examples.py
+++ helper/pydriller_examples.py
@@ -10,6 +10,23 @@ def traverse_commits():
     
     # for commit in Repository('https://github.com/jgraber/PythonFriday').traverse_commits():
     for commit in Repository('..\..\PythonFriday').traverse_commits():
+        print(f"{commit.committer_date} - {commit.hash[0:8]} - {commit.author.name}")
+        
+    # Sample output:
+    # 2021-10-21 22:33:00+02:00 - 1be83b81 - Johnny Graber
+    # 2021-10-22 23:30:00+02:00 - c816a116 - Johnny Graber
+    # 2021-10-23 23:15:00+02:00 - 4f379c7a - Johnny Graber
+    # 2021-10-24 11:18:10+02:00 - 64ec912d - Johnny Graber
+    # 2021-10-25 15:37:05+02:00 - 0a9b1109 - Johnny Graber
+
+def traverse_commits_and_files():
+    """
+    traverse_commits() allows us to walk through the commit history.
+    """
+    print_separator("traverse_commits")
+    
+    # for commit in Repository('https://github.com/jgraber/PythonFriday').traverse_commits():
+    for commit in Repository('..\..\PythonFriday').traverse_commits():
         print(f"{commit.hash} - {commit.committer_date} - {commit.author.name} - {commit.msg}")
         for file in commit.modified_files:
             print(f'  - {file.filename} has changed ({file.change_type.name})')
@@ -46,22 +63,22 @@ def count_commits():
     pp.pprint(files)
 
     # {'SQLAlchemy\\Core\\sqlalchemy_core_crud.py': 2,
-    #  'SQLAlchemy\\Core\\sqlalchemy_core_filters.py': 3,
-    #  'SQLAlchemy\\Core\\sqlalchemy_core_relationships.py': 1,
-    #  'SQLAlchemy\\ORM\\app_with_db.py': 2,
-    #  'SQLAlchemy\\ORM\\data\\__all_models.py': 2,
-    #  'SQLAlchemy\\ORM\\data\\author.py': 1,
-    #  'SQLAlchemy\\ORM\\data\\book.py': 2,
-    #  'SQLAlchemy\\ORM\\data\\book_author.py': 1,
-    #  'SQLAlchemy\\ORM\\data\\book_details.py': 1,
-    #  'SQLAlchemy\\ORM\\data\\db_session.py': 1,
-    #  'SQLAlchemy\\ORM\\data\\employee.py': 2,
-    #  'SQLAlchemy\\ORM\\data\\modelbase.py': 1,
-    #  'SQLAlchemy\\ORM\\data\\publisher.py': 1,
-    #  'SQLAlchemy\\ORM\\db\\Northwind_small.sqlite': 1,
-    #  'SQLAlchemy\\ORM\\sqlalchemy_orm_crud.py': 1,
-    #  'SQLAlchemy\\ORM\\sqlalchemy_orm_filters.py': 1,
-    #  'SQLAlchemy\\ORM\\sqlalchemy_orm_relationships.py': 1}
+    # 'SQLAlchemy\\Core\\sqlalchemy_core_filters.py': 3,
+    # 'SQLAlchemy\\Core\\sqlalchemy_core_relationships.py': 1,
+    # 'SQLAlchemy\\ORM\\app_with_db.py': 2,
+    # 'SQLAlchemy\\ORM\\data\\__all_models.py': 2,
+    # 'SQLAlchemy\\ORM\\data\\author.py': 1,
+    # 'SQLAlchemy\\ORM\\data\\book.py': 2,
+    # 'SQLAlchemy\\ORM\\data\\book_author.py': 1,
+    # 'SQLAlchemy\\ORM\\data\\book_details.py': 1,
+    # 'SQLAlchemy\\ORM\\data\\db_session.py': 1,
+    # 'SQLAlchemy\\ORM\\data\\employee.py': 2,
+    # 'SQLAlchemy\\ORM\\data\\modelbase.py': 1,
+    # 'SQLAlchemy\\ORM\\data\\publisher.py': 1,
+    # 'SQLAlchemy\\ORM\\db\\Northwind_small.sqlite': 1,
+    # 'SQLAlchemy\\ORM\\sqlalchemy_orm_crud.py': 1,
+    # 'SQLAlchemy\\ORM\\sqlalchemy_orm_filters.py': 1,
+    # 'SQLAlchemy\\ORM\\sqlalchemy_orm_relationships.py': 1}
 
 def count_contributors():
     """
