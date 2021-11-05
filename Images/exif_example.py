de865b5ae3131959bea64df7bc8661a46ebd3b4f
Johnny Graber <jg@jgraber.ch>
2021-11-05 20:35:00 +0100

    add conversion logic to turn GPS data into something useful

--- Images/exif_example.py
+++ Images/exif_example.py
@@ -1,6 +1,13 @@
 # pip install exif
 from exif import Image
 
+def decimal_coords(coords, ref):
+    '''from https://medium.com/spatial-data-science/how-to-extract-gps-coordinates-from-images-in-python-e66e542af354 '''
+ decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
+ if ref == 'S' or ref == 'W':
+     decimal_degrees = -decimal_degrees
+ return decimal_degrees
+
 with open('venezia.jpg', 'rb') as image_file:
     my_image = Image(image_file)
 
@@ -12,3 +19,7 @@ if(my_image.has_exif):
             print(getattr(my_image, x, '-'))
         except Exception:
             print("--")
+
+lat = decimal_coords(my_image.gps_latitude, my_image.gps_latitude_ref)
+long = decimal_coords(my_image.gps_longitude, my_image.gps_longitude_ref)
+print(f'Location: {lat}, {long}')
