ed24f27a7b6b4995a20a74d2e0cfb9fdb14affcb
Johnny Graber <jg@jgraber.ch>
2021-11-19 22:13:07 +0100

    Create explore_feedparser.py

---
+++ RSS/explore_feedparser.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from time import mktime
+import feedparser
+
+d = feedparser.parse('https://improveandrepeat.com/category/pythonfriday/feed/')
+
+print("Title: " + d.feed.title)
+print("Site: " + d.feed.link)
+print("Generator: " + d.feed.generator)
+print("Last updated: " +d.feed.updated)
+
+for post in d.entries:
+    dt = datetime.fromtimestamp(mktime(post.published_parsed)).strftime("%Y-%m-%d")
+    print(f"{post.title} ({dt}) - {post.link}")
+
