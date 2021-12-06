2a8fea200f95e2a396016c471bc3888a240013cf
Johnny Graber <jg@jgraber.ch>
2021-12-06 21:27:12 +0100

    Update explore_feedparser.py

--- RSS/explore_feedparser.py
+++ RSS/explore_feedparser.py
@@ -2,18 +2,18 @@ from datetime import datetime
 from time import mktime
 import feedparser
 
-d = feedparser.parse('https://improveandrepeat.com/category/pythonfriday/feed/')
+f = feedparser.parse('https://improveandrepeat.com/category/pythonfriday/feed/')
 
-print("Title: " + d.feed.title)
-print("Site: " + d.feed.link)
-print("Generator: " + d.feed.generator)
-print("Last updated: " +d.feed.updated)
+print("Title: " + f.feed.title)
+print("Site: " + f.feed.link)
+print("Generator: " + f.feed.generator)
+print("Last updated: " +f.feed.updated)
 
-for post in d.entries:
-    # dt = datetime.fromtimestamp(mktime(post.published_parsed)).strftime("%Y-%m-%d")
-    # print(f"{post.title} ({dt}) - {post.link}")
-    print(f"{post.title} ({post.published}) - {post.link}")
-    print(post.summary)
+for post in f.entries:
+    dt = datetime.fromtimestamp(mktime(post.published_parsed)).strftime("%Y-%m-%d")
+    print(f"{post.title} ({dt})")
+    # print(f"{post.title} ({post.published}) - {post.link}")
+    # print(post.summary)
     # for key in post.keys():
     #     print(key)
 
