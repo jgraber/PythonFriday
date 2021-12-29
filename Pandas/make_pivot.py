c48f38f1175c25a1f6e5144e965f4172c8099897
Johnny Graber <jg@jgraber.ch>
2021-12-29 16:27:34 +0100

    add total to columns and rows

--- Pandas/make_pivot.py
+++ Pandas/make_pivot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('input.csv', delimiter=';')
-pivot = df.pivot(index='project', columns='month', values='loc')
+# pivot = df.pivot(index='project', columns='month', values='loc')
+pivot = df.pivot_table(index='project', columns='month', values='loc', aggfunc = 'sum', fill_value = 'N/A', margins = True, margins_name='Total')
 pivot.to_csv('output.csv')
