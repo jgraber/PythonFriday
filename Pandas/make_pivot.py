9b8ad764c17dec1426f7bd10969d0d6c7aa1cda0
Johnny Graber <jg@jgraber.ch>
2021-12-30 21:34:12 +0100

    Update make_pivot.py

--- Pandas/make_pivot.py
+++ Pandas/make_pivot.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('input.csv', delimiter=';')
+# create a pivot with just the data:
 # pivot = df.pivot(index='project', columns='month', values='loc')
+# create a pivot with a total per column and per row:
 pivot = df.pivot_table(index='project', columns='month', values='loc', aggfunc = 'sum', fill_value = 'N/A', margins = True, margins_name='Total')
 pivot.to_csv('output.csv')
