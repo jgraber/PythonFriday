487b8661c4aeec3d4b597dbfc48d2c9ea4f6a6c4
Johnny Graber <jg@jgraber.ch>
2021-12-31 23:35:37 +0100

    Update convert_delimiter.py

--- Pandas/convert_delimiter.py
+++ Pandas/convert_delimiter.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 # reads file with ; as delimiter
 df = pd.read_csv('input.csv', delimiter=';')
+print(df)
 # writes file with , as delimiter (, is default value)
 df.to_csv('output_converted.csv', index=False)
