6e6f311e4f97bcd45777ee91a63bf4ea6c587bc3
Johnny Graber <jg@jgraber.ch>
2022-01-03 21:44:32 +0100

    Update env_reader.py

--- Twitter/env_reader.py
+++ Twitter/env_reader.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 database_url = os.getenv('DATABASE_URL')
 print(database_url)
 
+# loads .env file into application environment
 load_dotenv()
 
 database_url = os.getenv('DATABASE_URL', 'sqlite://')
