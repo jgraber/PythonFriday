87e618dbb67b2524c7e12389284144acd3dcf8a8
Johnny Graber <jg@jgraber.ch>
2022-01-07 18:48:12 +0100

    add comments to pivot creator

--- Pandas/make_pivot.py
+++ Pandas/make_pivot.py
@@ -1,8 +1,29 @@
 import pandas as pd
 
+# df = pd.read_csv('input.csv', delimiter=';')
+# # create a pivot with just the data:
+# # pivot = df.pivot(index='project', columns='month', values='loc')
+# # create a pivot with a total per column and per row:
+# pivot = df.pivot_table(index='project', columns='month', values='loc', aggfunc = 'sum', fill_value = 'N/A', margins = True, margins_name='Total')
+# pivot.to_csv('output.csv')
+# load the data into a data frame
 df = pd.read_csv('input.csv', delimiter=';')
+
 # create a pivot with just the data:
-# pivot = df.pivot(index='project', columns='month', values='loc')
-# create a pivot with a total per column and per row:
-pivot = df.pivot_table(index='project', columns='month', values='loc', aggfunc = 'sum', fill_value = 'N/A', margins = True, margins_name='Total')
+pivot = df.pivot(index='project', columns='month', values='loc')
+print(pivot)
+
+# export pivot to CSV
 pivot.to_csv('output.csv')
+
+
+print("\n-------\n")
+
+# create a pivot with a grand total column & row
+pt = df.pivot_table(index='project', columns='month', values='loc', 
+                    aggfunc = 'sum', fill_value = 'N/A', 
+                    margins = True, margins_name='Total')
+print(pt)
+
+# export pivot table to CSV
+pt.to_csv('output_table.csv')
