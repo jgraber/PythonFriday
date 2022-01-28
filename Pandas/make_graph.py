c6f49ddecec7c4f78600de0f262bdc16d6077952
Johnny Graber <jg@jgraber.ch>
2022-01-28 20:24:05 +0100

    change type of pandas column

--- Pandas/make_graph.py
+++ Pandas/make_graph.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('input.csv', delimiter=';')
+df = df.astype({"month": str})
 pivot = df.pivot(index='month', columns='project', values='loc')
 print(pivot)
 
