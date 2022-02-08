591de27d437ec8de12080a23383ad7c1531baf20
Johnny Graber <jg@jgraber.ch>
2022-02-08 21:52:44 +0100

    better output for set routines examples

--- Pandas/set_routines.py
+++ Pandas/set_routines.py
@@ -4,19 +4,18 @@ import numpy as np
 a = [1,2,3,4]
 b = [3,4,5,6]
 
-print(type(a))
+print(f"a: {a}")
+print(f"b: {b}")
+
 
 in_a_not_in_b = np.setdiff1d(a,b) 
-print(in_a_not_in_b)
+print(f"in a but not in b: {in_a_not_in_b}")
 
 in_b_not_in_a = np.setdiff1d(b,a) 
-print(in_b_not_in_a)
+print(f"in b but not in a: {in_b_not_in_a}")
 
 intersect = np.intersect1d(a,b)
-print(intersect)
+print(f"both in a AND b: {intersect}")
 
 union = np.union1d(a,b)
-print(union)
-
-unique = np.unique(a,b)
-print(unique)
+print(f"everything from a and b: {union}")
