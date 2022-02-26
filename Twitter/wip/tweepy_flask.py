071ea90a4b2546aeb3daa02ddfabfd58925e99f6
Johnny Graber <jg@jgraber.ch>
2022-02-26 20:29:56 +0100

    add example on how to tweet from Flask

--- Twitter/wip/tweepy_flask.py
+++ Twitter/wip/tweepy_flask.py
@@ -1,6 +1,7 @@
 import flask
 from flask import redirect, request
 import tweepy
+import random
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -29,6 +30,22 @@ def callback():
     user_tokens = f"access-token={auth.access_token}<br>access-token-secret={auth.access_token_secret}"
     return user_tokens
     
+
+@app.route('/tweet')
+def tweet():
+    # You would read these values from the session
+    user_token = os.getenv('access-token')
+    user_token_secret = os.getenv('access-token-secret')
+
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(user_token,user_token_secret)
+    api = tweepy.API(auth)
+
+    # Create a tweet - random() to not write same tweet twice
+    api.update_status(f"A Tweet from Flask - {random.random()}")
+
+    return "Tweet send with Flask"
+
 
 if(__name__ == "__main__"):
     app.run(port=6006)
