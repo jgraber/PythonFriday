0a161d4f1760d1b67081dc91a1aceacae898a4a2
Johnny Graber <jg@jgraber.ch>
2022-03-06 17:32:26 +0100

    add examples for streaming search results with Tweepy

---
+++ Twitter/stream_search_results_V1.py
@@ -0,0 +1,25 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+consumer_key = os.getenv('api-key')
+consumer_secret = os.getenv('api-key-secret')
+access_token = os.getenv('access-token')
+access_token_secret = os.getenv('access-token-secret')
+
+# Subclass tweepy.Stream to print the tweets
+class TweetPrinter(tweepy.Stream):
+
+    def on_status(self, status):
+        print('-'*50)
+        print(f"{status.id} {status.user.name} (@{status.user.screen_name}) at {status.created_at} {status.text}")
+
+# Initialize instance of the subclass
+printer = TweetPrinter(
+  consumer_key, consumer_secret,
+  access_token, access_token_secret
+)
+
+# Filter real-time Tweets by keyword
+printer.filter(track=["#Python"])
---
+++ Twitter/stream_search_results_V2.py
@@ -0,0 +1,36 @@
+import tweepy
+from tweepy import StreamingClient, StreamRule
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+bearer_token = os.getenv('bearer-token')
+
+class TweetPrinterV2(tweepy.StreamingClient):
+    users = {}
+
+    def on_tweet(self, tweet):
+        print(f"{tweet.id} {tweet.created_at} ({tweet.author_id})): {tweet.text}")
+        print("-"*50)
+
+printer = TweetPrinterV2(bearer_token)
+
+# clean-up pre-existing rules
+rule_ids = []
+result = printer.get_rules()
+for rule in result.data:
+    print(f"rule marked to delete: {rule.id} - {rule.value}")
+    rule_ids.append(rule.id)
+
+if(len(rule_ids) > 0):
+    printer.delete_rules(rule_ids)
+    printer = TweetPrinterV2(bearer_token)
+else:
+    print("no rules to delete")
+
+# add new rules    
+# rule = StreamRule(value="Python")
+rule = StreamRule(value="NumPy")
+printer.add_rules(rule)
+
+printer.filter(expansions="author_id",tweet_fields="created_at")
