f16adcb34389154cc093f3d3add42b984be451a0
Johnny Graber <jg@jgraber.ch>
2022-04-11 20:20:00 +0200

    add __main__

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -1,8 +1,13 @@
 # Example from https://docs.celeryq.dev/en/stable/getting-started/first-steps-with-celery.html#first-steps
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+# app = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost//')
+app = Celery('tasks', broker='amqp://guest@localhost//')
 
 @app.task
 def add(x, y):
+    print(f"{x} + {y}")
     return x + y
+
+if __name__ == '__main__':
+    app.worker_main()
