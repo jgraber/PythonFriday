f21757ff2e68d9199634853ca3ca3c6e51d4e411
Johnny Graber <jg@jgraber.ch>
2022-04-13 20:22:00 +0200

    add logger

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -1,12 +1,13 @@
 # Example from https://docs.celeryq.dev/en/stable/getting-started/first-steps-with-celery.html#first-steps
 from celery import Celery
+import logging
 
 app = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost//')
 # app = Celery('tasks', broker='amqp://guest@localhost//')
 
 @app.task
 def add(x, y):
-    print(f"{x} + {y}")
+    logging.critical(f"{x} + {y}")
     return x + y
 
 if __name__ == '__main__':
