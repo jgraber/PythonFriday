40000ad35ba804ce2415a75caf7d6645b059d964
Johnny Graber <jg@jgraber.ch>
2022-04-14 22:03:00 +0200

    try SQLAlchemy as backend

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -2,10 +2,10 @@
 from celery import Celery
 import logging
 
-app = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost//')
+app = Celery('tasks', backend='db+sqlite:///./celery.db', broker='amqp://guest@localhost//')
 # app = Celery('tasks', broker='amqp://guest@localhost//')
 
-@app.task
+@app.task(bind=True)
 def add(x, y):
     logging.critical(f"{x} + {y}")
     return x + y
