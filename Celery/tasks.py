793ef1a6440ba920906b306745152251f14744e1
Johnny Graber <jg@jgraber.ch>
2022-04-19 20:55:00 +0200

    add rate limit for task

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -4,6 +4,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 app = Celery('tasks', backend='db+sqlite:///./celery.db', broker='amqp://guest@localhost//')
+app.control.rate_limit('tasks.add', '1/m')
 # app = Celery('tasks', broker='amqp://guest@localhost//')
 
 @app.task
