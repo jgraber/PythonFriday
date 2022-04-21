e7567a918d36d4d0af0aa60cc566d2238ac6d461
Johnny Graber <jg@jgraber.ch>
2022-04-21 22:04:43 +0200

    fix formating

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -3,17 +3,21 @@ from celery import Celery
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
+
 app = Celery('tasks', backend='db+sqlite:///./celery.db', broker='amqp://guest@localhost//')
 app.control.rate_limit('tasks.add', '1/m')
+
 
 @app.task
 def add(x, y):
     logger.critical(f"{x} + {y}")
     return x + y
 
+
 @app.task
 def reverse(text):
     return text[::-1]
+
 
 if __name__ == '__main__':
     app.worker_main()
