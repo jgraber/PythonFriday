4c42c5893e088eaaed0661f53d51c37efc1b5abc
Johnny Graber <jg@jgraber.ch>
2022-05-01 23:48:29 +0200

    add logger

--- Celery/celery_task.py
+++ Celery/celery_task.py
@@ -1,12 +1,15 @@
 import time
 from celery import Celery
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
 app = Celery('tasks', broker='amqp://guest@localhost//')
 app.control.rate_limit('celery_task.prepare', '1/m')
 
 @app.task
 def prepare(order_id):
     time.sleep(5) # simulate work
-    print(f"order #{order_id} prepared")
+    logger.info(f"order #{order_id} prepared")
 
 if __name__ == '__main__':
     app.worker_main()
