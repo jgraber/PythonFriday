e352888fa9a897ce9f9172d6ed570b4c326eb314
Johnny Graber <jg@jgraber.ch>
2022-09-06 21:23:00 +0200

    add method to read the sitemap

--- Web/basic_link_checker.py
+++ Web/basic_link_checker.py
@@ -6,3 +6,14 @@ from typing import NamedTuple
 class Page(NamedTuple):
     url: str
     text: str
+
+
+def read_sitemap(domain):
+    tree = sitemap_tree_for_homepage(domain)
+    pages = []
+
+    for page in tree.all_pages():
+        pages.append(page.url)
+    
+    return pages
+
