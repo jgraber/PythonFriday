ae4adfc5f262c10224b74a48e467f99fbca9126c
Johnny Graber <jg@jgraber.ch>
2022-09-08 20:57:00 +0200

    add method to find links on pages

--- Web/basic_link_checker.py
+++ Web/basic_link_checker.py
@@ -18,3 +18,30 @@ def read_sitemap(domain):
     
     return pages
 
+
+def find_links(pages):
+    all_links = []
+
+    for page in pages:
+        content = requests.get(page)
+        soup = BeautifulSoup(content.text, 'html.parser')
+        links = soup.find_all("a")
+
+        for link in links:
+            if link.get('href') == "#":
+                continue
+
+            if  link.get('rel') is not None and 'nofollow' in link.get('rel'):
+                continue
+
+            link_text = link.get_text().strip()
+            link_target = link.get('href')
+
+            if not link_target.lower().startswith("http"):
+                link_target = page + link_target
+
+            all_links.append(Page(page, link_text, link_target))
+    
+    return all_links
+
+
