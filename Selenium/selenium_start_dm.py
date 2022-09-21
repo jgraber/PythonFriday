237cba32b8a60677f503cb2b901e147636940afa
Johnny Graber <jg@jgraber.ch>
2022-09-21 23:12:07 +0200

    Create selenium_start_dm.py

---
+++ Selenium/selenium_start_dm.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+import time
+import os
+from dotenv import load_dotenv
+
+# read value for GH_TOKEN key from .evn file
+load_dotenv()
+
+# https://github.com/SergeyPirogov/webdriver_manager#gh_token
+# ValueError: API Rate limit exceeded. You have to add GH_TOKEN!!!
+# service = Service(executable_path="./geckodriver.exe")
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.implicitly_wait(1)
+
+driver.get("https://duckduckgo.com/?t=ha&va=j")
+title = driver.title
+print(title)
+
+time.sleep(5)
+
+driver.quit()
