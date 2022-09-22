a8be6dc01bd81acd50b20b484c2f38724a7d0fd6
Johnny Graber <jg@jgraber.ch>
2022-09-22 20:03:46 +0200

    Create selenium_start.py

---
+++ Selenium/selenium_start.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+import time
+
+service = Service(executable_path="./geckodriver.exe")
+driver = webdriver.Firefox(service=service)
+driver.implicitly_wait(1)
+
+driver.get("https://duckduckgo.com/?t=ha&va=j")
+title = driver.title
+print(title)
+
+time.sleep(5)
+
+driver.quit()
