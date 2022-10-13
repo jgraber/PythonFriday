1cd155a85a8db043d26632b071d48ebe18f46010
Johnny Graber <jg@jgraber.ch>
2022-10-13 22:20:12 +0200

    format code for blog post

--- Selenium/selenium_automate.py
+++ Selenium/selenium_automate.py
@@ -66,8 +66,12 @@ print(title)
 
 print(50 * '=')
 
-results_div = driver.find_element(by=By.CLASS_NAME, value="results")
-results = results_div.find_elements(by=By.TAG_NAME, value="h2")
+results_div = driver.find_element(
+    by=By.CLASS_NAME, 
+    value="results")
+results = results_div.find_elements(
+    by=By.TAG_NAME, 
+    value="h2")
 
 for result in results:
     print(f"* {result.text}")
