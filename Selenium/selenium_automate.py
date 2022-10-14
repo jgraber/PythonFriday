217d8f678a8b814a165e770a0ef60473df0e2e2e
Johnny Graber <jg@jgraber.ch>
2022-10-14 22:46:36 +0200

    explicit wait on element

--- Selenium/selenium_automate.py
+++ Selenium/selenium_automate.py
@@ -90,6 +90,19 @@ driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
 print(50 * '=')
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "mySlowElement"))
+    )
+except TimeoutException as error:
+    print(f"{type(error)}: {error}")
+
+title = driver.title
+print(title)
 
 time.sleep(15)
 
