7971a715b2872d98ed1092f9bdb79cb0e865c9a7
Johnny Graber <jg@jgraber.ch>
2022-10-19 21:25:55 +0200

    add login method

--- Selenium/selenium_jetpack.py
+++ Selenium/selenium_jetpack.py
@@ -16,6 +16,36 @@ def prepare_browser():
     return driver
 
 
+def login(driver):
+    # go to statistik page to get correct redirect to login mask
+    driver.get(os.getenv('login_page'))
+
+    # fill username field
+    username = driver.find_element(
+        by=By.ID, 
+        value="usernameOrEmail")
+    username.send_keys(os.getenv('user'))
+    cont = driver.find_element(
+        by=By.CLASS_NAME, 
+        value="form-button")
+    cont.click()
+    
+    time.sleep(2)
+
+    # fill password field
+    password = driver.find_element(
+        by=By.ID, 
+        value="password")
+    password.send_keys(os.getenv('password'))
+    submit = driver.find_element(
+        by=By.CLASS_NAME, 
+        value="form-button")
+    submit.click()
+
+    # wait a moment to finish login
+    time.sleep(2)
+
+
 if __name__ == '__main__':
     load_dotenv()
     driver = prepare_browser()
