1a9226ed852f7a8a4ef827c33be63b2758713c40
Johnny Graber <jg@jgraber.ch>
2022-10-26 21:59:00 +0200

    reuse test code

--- Selenium/selenium_grid_demo.py
+++ Selenium/selenium_grid_demo.py
@@ -9,3 +9,32 @@ driver = webdriver.Remote(
 )
 driver.get("https://duckduckgo.com/?t=ha&va=j")
 
+# time.sleep(15)
+
+search_box = driver.find_element(
+    by=By.ID, 
+    value="search_form_input_homepage")
+search_box.send_keys("Selenium web")
+
+search_button = driver.find_element(
+    by=By.ID, 
+    value="search_button_homepage")
+search_button.click()
+
+title = driver.title
+print(title)
+
+results_div = driver.find_element(
+    by=By.CLASS_NAME, 
+    value="results")
+results = results_div.find_elements(
+    by=By.TAG_NAME, 
+    value="h2")
+
+for result in results:
+    print(f"* {result.text}")
+
+time.sleep(15)
+
+driver.quit() 
+  
