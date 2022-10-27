fe6734557fa7d713e62f7602e5052005f1a96e35
Johnny Graber <jg@jgraber.ch>
2022-10-27 19:01:00 +0200

    add Edge driver

--- Selenium/selenium_grid_demo.py
+++ Selenium/selenium_grid_demo.py
@@ -2,10 +2,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-firefox_options = webdriver.FirefoxOptions()
+# firefox_options = webdriver.FirefoxOptions()
+# driver = webdriver.Remote(
+#     command_executor='http://localhost:4444',
+#     options=firefox_options
+# )
+
+edge_options = webdriver.EdgeOptions()
 driver = webdriver.Remote(
     command_executor='http://localhost:4444',
-    options=firefox_options
+    options=edge_options
+)
+
 )
 driver.get("https://duckduckgo.com/?t=ha&va=j")
 
