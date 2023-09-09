83e5d45c450ff4655c7aadfcd0b15453a9b83435
Johnny Graber <jg@jgraber.ch>
2023-09-09 23:42:13 +0200

    Update explore_feedparser.py

--- RSS/explore_feedparser.py
+++ RSS/explore_feedparser.py
@@ -6,12 +6,12 @@ f = feedparser.parse('https://improveandrepeat.com/category/pythonfriday/feed/')
 
 print("Title: " + f.feed.title)
 print("Site: " + f.feed.link)
-print("Generator: " + f.feed.generator)
+#print("Generator: " + f.feed.generator)
 print("Last updated: " +f.feed.updated)
 
-for post in f.entries:
+for post in reversed(f.entries):
     dt = datetime.fromtimestamp(mktime(post.published_parsed)).strftime("%Y-%m-%d")
-    print(f"{post.title} ({dt})")
+    print(f'\t<li><a href="{post.link}">{post.title} ({dt})</a></li>')
     # print(f"{post.title} ({post.published}) - {post.link}")
     # print(post.summary)
     # for key in post.keys():
