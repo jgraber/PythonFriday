f3cdd4eff13aefdeaad49fe2a71f4ebda7fa75f7
Johnny Graber <jg@jgraber.ch>
2023-11-28 22:23:50 +0100

    Create rounding_example.py

---
+++ Python/rounding_example.py
@@ -0,0 +1,14 @@
+
+print(f"round({3.4}) is {round(3.4)}")
+print(f"round({3.5}) is {round(3.5)}")
+print(f"round({3.51}) is {round(3.51)}")
+
+print(f"round({4.51}) is {round(4.51)}")
+
+print(f"round({4.5}) is {round(4.5)}")
+
+print("-" * 30)
+
+for i in range(1, 10):
+    number = i + 0.5
+    print(f"round({number}) is {round(number)}")
