77902d6c065cad1aed1975904f5fb8c33896c505
Johnny Graber <jg@jgraber.ch>
2023-12-14 11:40:06 +0100

    Create cache_demo.py

---
+++ Basics/cache_demo.py
@@ -0,0 +1,83 @@
+# no cache
+
+def factorial(n):
+    print(n)
+    return n * factorial(n-1) if n else 1
+
+
+factorial(5)
+factorial(6)
+factorial(8)
+
+
+# with cache
+from functools import cache
+@cache
+def factorial(n):
+    print(n)
+    return n * factorial(n-1) if n else 1
+
+
+factorial(5)
+factorial(6)
+factorial(8)
+
+
+"""
+>>> from functools import cache
+>>> def factorial(n):
+...     print(n)
+...     return n * factorial(n-1) if n else 1
+...
+>>>
+>>> factorial(5)
+5
+4
+3
+2
+1
+0
+120
+>>> factorial(6)
+6
+5
+4
+3
+2
+1
+0
+720
+>>> factorial(8)
+8
+7
+6
+5
+4
+3
+2
+1
+0
+40320
+
+>>> from functools import cache
+>>> @cache
+... def factorial(n):
+...     print(n)
+...     return n * factorial(n-1) if n else 1
+...
+>>> factorial(5)
+5
+4
+3
+2
+1
+0
+120
+>>> factorial(6)
+6
+720
+>>> factorial(8)
+8
+7
+40320
+"""
