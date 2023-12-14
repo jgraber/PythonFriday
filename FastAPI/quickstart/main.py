7ed0e34b62dc6dfc53b393c1ac96d4eb0bf2e4fb
Johnny Graber <jg@jgraber.ch>
2023-12-15 00:11:45 +0100

    Create main.py

---
+++ FastAPI/quickstart/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# Installation: pip install fastapi && pip install "uvicorn[standard]"
+# Start: uvicorn main:app --reload
+# Endpoints:
+# http://127.0.0.1:8000/
+# http://127.0.0.1:8000/items/1?q=Apples
+# http://127.0.0.1:8000/docs
