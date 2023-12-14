e2ddea0aabc4c048644fef81daa0194e162c8fbb
Johnny Graber <jg@jgraber.ch>
2023-12-15 00:11:18 +0100

    Create regex_demo.py

---
+++ RegEx/regex_demo.py
@@ -0,0 +1,88 @@
+import re
+res = re.search("123","123abc123")
+type(res)
+# <class 're.Match'>
+res = re.search("1234","123abc123")
+type(res)
+# <class 'NoneType'>
+
+res = re.findall("123","123abc123")
+res
+type(res)
+# <class 'list'>
+type(res[0])
+# <class 'str'>
+
+res = re.finditer("123","123abc123")
+for i in res:
+    print(i)
+
+# <re.Match object; span=(0, 3), match='123'>
+# <re.Match object; span=(6, 9), match='123'>
+
+
+
+"""
+>>> import re
+>>> text = "123abc123"
+>>> re.search("123",text)
+<re.Match object; span=(0, 3), match='123'>
+>>> re.search("1234",text)
+>>> res = re.search("1234",text)
+>>> type(res)
+<class 'NoneType'>
+>>> res = re.search("1234",text)
+>>> type(res)
+<class 'NoneType'>
+>>> res = re.search("123",text)
+>>> type(res)
+<class 're.Match'>
+>>> res = re.search("1234",text)
+>>> type(res)
+<class 'NoneType'>
+>>> res = re.findall("123",text)
+>>> res
+['123', '123']
+>>> type(res)
+<class 'list'>
+>>> help(re.findall)
+Help on function findall in module re:
+
+findall(pattern, string, flags=0)
+    Return a list of all non-overlapping matches in the string.
+
+    If one or more capturing groups are present in the pattern, return
+    a list of groups; this will be a list of tuples if the pattern
+    has more than one group.
+
+    Empty matches are included in the result.
+
+>>> res = re.findall("1234",text)
+>>> type(res)
+<class 'list'>
+>>> res
+[]
+>>> res = re.finditer("123",text)
+>>> for i in res:
+...     print(i)
+...
+<re.Match object; span=(0, 3), match='123'>
+<re.Match object; span=(6, 9), match='123'>
+>>> res = re.findall("1234",text)
+>>> type(res[0])
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+IndexError: list index out of range
+>>> res = re.findall("123",text)
+>>> type(res[0])
+<class 'str'>
+>>> res = re.match("123",text)
+>>> res
+<re.Match object; span=(0, 3), match='123'>
+>>> res = re.match("abc",text)
+>>> res
+>>> res = re.fullmatch("123",text)
+>>> res
+>>> res = re.fullmatch("123abc123",text)
+>>> res
+<re.Match object; span=(0, 9), match='123abc123'>"""
