fa0e1abf68c7aa159f344e11744f731ebfca529f
Johnny Graber <jg@jgraber.ch>
2024-02-02 19:01:34 +0100

    Create gc_demo.py

---
+++ Python/gc_demo.py
@@ -0,0 +1,25 @@
+import gc
+import time
+
+class Student:
+    def __init__(self, name):
+        self._name = name
+
+
+j = Student("Johnny")
+print(gc.is_tracked(j))
+
+
+start = time.perf_counter()
+students = []
+for i in range(1, 10000000):
+    x = Student(str(i))
+    students.append(x)
+elapsed = time.perf_counter() - start
+print(f"GC untouched executed in {elapsed:0.2f} seconds.")
+
+print(gc.get_stats())
+print(gc.get_count())
+print(gc.get_threshold())
+print(len(students))
+
