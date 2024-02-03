26eb70310b1e86669c783c5991d61bcfc62e08cc
Johnny Graber <jg@jgraber.ch>
2024-02-03 14:05:48 +0100

    add example with changed GC threshold

--- Python/gc_demo.py
+++ Python/gc_demo.py
@@ -23,3 +23,18 @@ print(gc.get_count())
 print(gc.get_threshold())
 print(len(students))
 
+
+gc.set_threshold(50000, 100, 100)
+start = time.perf_counter()
+students = []
+for i in range(1, 10000000):
+    x = Student(str(i))
+    students.append(x)
+elapsed = time.perf_counter() - start
+print(f"GC modified executed in {elapsed:0.2f} seconds.")
+
+print(gc.get_stats())
+print(gc.get_count())
+print(gc.get_threshold())
+print(len(students))
+
