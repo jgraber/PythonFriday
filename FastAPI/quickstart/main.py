94986b9f0edb15241bad5b5de300537ed7f0dbe2
Johnny Graber <jg@jgraber.ch>
2024-02-06 17:46:28 +0100

    switch to async methods

--- FastAPI/quickstart/main.py
+++ FastAPI/quickstart/main.py
@@ -4,12 +4,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 # Installation: pip install fastapi && pip install "uvicorn[standard]"
