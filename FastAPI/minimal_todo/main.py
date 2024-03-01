6601443eba7e604b0e05d2a5aaf18e89cb53c1e0
Johnny Graber <jg@jgraber.ch>
2024-03-01 15:33:49 +0100

    use DataStore in API

--- FastAPI/minimal_todo/main.py
+++ FastAPI/minimal_todo/main.py
@@ -1,30 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from .models.todo import *
+from .data.datastore import DataStore
 
 app = FastAPI()
 
-db = []
+db = DataStore()
 
 @app.post("/api/todo")
 async def create_task(task: TaskInput):
-    pos = len(db) + 1
-    result = TaskOutput(id=pos, 
-                        name=task.name, 
-                        priority=task.priority, 
-                        due_date=task.due_date,
-                        done=task.done, 
-                        created_at=date.today())
-    db.append(result)
+    result = db.add(task)
     return result
 
 
 @app.get("/api/todo/{id}")
 async def show_task(id: int):
-    result = [item for item in db if item.id == id]
-    # return result[0]
+    result = db.get(id)
 
-    if len(result) > 0:
-        return result[0]
+    if result:
+        return result
     else:
         raise HTTPException(status_code=404, detail="Task not found")
     
@@ -32,23 +25,13 @@ async def show_task(id: int):
 
 @app.put("/api/todo/{id}")
 async def update_task(id: int, task: TaskInput):
-    result = [item for item in db if item.id == id]
-
-    if len(result) == 1:
-        current = result[0]
-        current.name = task.name
-        current.priority = task.priority
-        current.due_date = task.due_date
-        current.done = task.done
-
-        return current
-    else:
+    try:
+        result = db.update(id, task)
+        return result
+    except ValueError:
         raise HTTPException(status_code=404, detail="Task not found")
     
 
 @app.delete("/api/todo/{id}")
 async def delete_task(id: int):
-    result = [item for item in db if item.id == id]
-
-    if len(result) == 1:
-        db.remove(result[0])
+    db.delete(id)
