e80e83618cf946b7522a106b94a798993b0f5d95
Johnny Graber <jg@jgraber.ch>
2024-04-16 19:27:19 +0200

    extract todo endpoints into own router

--- FastAPI/minimal_todo/main.py
+++ FastAPI/minimal_todo/main.py
@@ -1,66 +1,12 @@
-from typing import List
-from fastapi import Depends, FastAPI, HTTPException, Request, Response, status
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from .models.todo import *
-from .data.datastore import DataStore
+from fastapi import FastAPI
+from .routers import todo
+
 
 app = FastAPI()
-
-db = DataStore()
-
-async def filter_parameters(q: str | None = None, 
-                            include_done: bool = True, 
-                            due_before: date = date.today() + timedelta(days=365)):
-    return {"q": q, "include_done": include_done, "due_before": due_before }
+app.include_router(todo.router)
 
 
 @app.get("/", include_in_schema=False)
 async def main():
     return {'message':'The minimalistic ToDo API'}
 
-
-@app.get("/api/todo")
-async def show_all_tasks(filter: Annotated[dict, Depends(filter_parameters)]) -> List[TaskOutput]:
-    result = db.all()
-
-    if not filter["include_done"]:
-        result = [item for item in result if item.done == False ]
-
-    result = [item for item in result if item.due_date <= filter["due_before"] ]
-
-    return result
-
-@app.post("/api/todo", status_code=status.HTTP_201_CREATED)
-async def create_task(task: TaskInput, request: Request) -> TaskOutput:
-    result = db.add(task)
-    headers = {"Location": f"{request.base_url}api/todo/{result.id}"}
-    return JSONResponse(content=jsonable_encoder(result), 
-                        status_code=status.HTTP_201_CREATED,
-                        headers=headers)
-
-
-@app.get("/api/todo/{id}")
-async def show_task(id: int) -> TaskOutput:
-    result = db.get(id)
-
-    if result:
-        return result
-    else:
-        raise HTTPException(status_code=404, detail="Task not found")
-    
-
-
-@app.put("/api/todo/{id}")
-async def update_task(id: int, task: TaskInput) -> TaskOutput:
-    try:
-        result = db.update(id, task)
-        return result
-    except ValueError:
-        raise HTTPException(status_code=404, detail="Task not found")
-    
-
-@app.delete("/api/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_task(id: int) -> None:
-    db.delete(id)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ FastAPI/minimal_todo/routers/todo.py
@@ -0,0 +1,64 @@
+from datetime import date, timedelta
+from typing import Annotated, List
+
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+from ..models.todo import TaskOutput, TaskInput
+from ..data.datastore import DataStore
+
+router = APIRouter()
+
+db = DataStore()
+
+async def filter_parameters(q: str | None = None, 
+                            include_done: bool = True, 
+                            due_before: date = date.today() + timedelta(days=365)):
+    return {"q": q, "include_done": include_done, "due_before": due_before }
+
+
+@router.get("/api/todo")
+async def show_all_tasks(filter: Annotated[dict, Depends(filter_parameters)]) -> List[TaskOutput]:
+    result = db.all()
+
+    if not filter["include_done"]:
+        result = [item for item in result if item.done == False ]
+
+    result = [item for item in result if item.due_date <= filter["due_before"] ]
+
+    return result
+
+
+@router.post("/api/todo", status_code=status.HTTP_201_CREATED)
+async def create_task(task: TaskInput, request: Request) -> TaskOutput:
+    result = db.add(task)
+    headers = {"Location": f"{request.base_url}api/todo/{result.id}"}
+    return JSONResponse(content=jsonable_encoder(result), 
+                        status_code=status.HTTP_201_CREATED,
+                        headers=headers)
+
+
+@router.get("/api/todo/{id}")
+async def show_task(id: int) -> TaskOutput:
+    result = db.get(id)
+
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+
+@router.put("/api/todo/{id}")
+async def update_task(id: int, task: TaskInput) -> TaskOutput:
+    try:
+        result = db.update(id, task)
+        return result
+    except ValueError:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+
+@router.delete("/api/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(id: int) -> None:
+    db.delete(id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
