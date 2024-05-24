3a4cfd8d342dab48ae6e6c03d134b5f4abc22701
Johnny Graber <jg@jgraber.ch>
2024-05-24 20:35:30 +0200

    extract db_file() method

--- FastAPI/extended_todo/dependencies.py
+++ FastAPI/extended_todo/dependencies.py
@@ -1,21 +1,27 @@
 import os
 
-from .data.database import create_async_session_factory, create_session_factory
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
+from .data.entities import User
+from .data.database import create_async_session_factory
 from .data.datastore_db import DataStoreDb
+
+
+def db_file():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        '.',
+        'db',
+        'todo_api.sqlite')
+    return db_file
 
 
 async def get_db():
     """
     Creates the datastore 
     """
-    db_file = os.path.join(
-        os.path.dirname(__file__),
-        '.',
-        'db',
-        'todo_api.sqlite')
     
-    factory = await create_async_session_factory(db_file)
+    factory = await create_async_session_factory(db_file())
     db = DataStoreDb(factory)
 
     yield db
-    
