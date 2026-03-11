bd2c97650af89c8e06cf0bc0e2c3db9ac9f04c55
Johnny Graber <jg@jgraber.ch>
2026-03-11 21:14:52 +0100

    refactor indexer

--- AI/pythonfriday_rag/pf_rag_index.py
+++ AI/pythonfriday_rag/pf_rag_index.py
@@ -11,8 +11,57 @@ from chromadb.config import Settings
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
 
+def get_markdown_splitter():
+    """ Create a MarkdownHeaderTextSplitter with the header list to split on. """
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
+
+    return MarkdownHeaderTextSplitter(headers_to_split_on)
+
+
+def process_files(path, markdown_splitter):
+    """ Find file(s) to extract data from """
+    document_splits = []  
+    
+    if path.is_dir():
+        md_files = sorted(path.rglob("*.md"))
+        for md_file in md_files:
+            print(md_file)
+            document_splits.extend(split_file(md_file, markdown_splitter))
+
+    elif path.is_file():
+        print(path)
+        document_splits.extend(split_file(path, markdown_splitter))
+
+    else:
+        raise FileNotFoundError(f"Path does not exist or is not a file/directory: {path}")
+    
+    
+    return document_splits
+
+
+def split_file(path, splitter):
+    """ Splits a file into parts and adds metadata. """
+    with open(path, 'r', encoding="utf-8") as file_content:
+        document = file_content.read()
+    
+    basic_metadata, document = process_markdown_metadata(document)
+    md_splits = splitter.split_text(document)
+    
+    part = 1
+    for entry in md_splits:
+        entry.metadata = add_reference(entry.metadata) | basic_metadata
+        entry.metadata["Id"] = entry.metadata["No"] + "-" + str(part)
+        part += 1
+        
+    return md_splits
+
+
 def process_markdown_metadata(md_text: str):
-    """Extract the metadata in the header and replace it with the title as level 1 header."""
+    """ Extract the metadata in the header and replace it with the title as level 1 header. """
     
     # Extract header
     header_match = re.search(r"^---\n(.*?)\n---", md_text, re.DOTALL)
@@ -79,7 +128,7 @@ def process_markdown_metadata(md_text: str):
 
 
 def add_reference(d: dict) -> dict:
-    """Generate a reference out of Header 1 / Header 2 sections"""
+    """ Generate a reference out of Header 1 / Header 2 sections """
     parts = []
     # pprint.pp(d)
     if "Header 1" in d:
@@ -93,68 +142,12 @@ def add_reference(d: dict) -> dict:
     return d
 
 
-def get_markdown_splitter():
-    """"Create a MarkdownHeaderTextSplitter with the header list to split on."""
-    headers_to_split_on = [
-        ("#", "Header 1"),
-        ("##", "Header 2"),
-        ("###", "Header 3"),
-    ]
-
-    return MarkdownHeaderTextSplitter(headers_to_split_on)
-
-
-def split_file(path, splitter):
-    """Splits a file into parts and adds metadata."""
-    with open(path, 'r', encoding="utf-8") as file_content:
-        document = file_content.read()
-    
-    basic_metadata, document = process_markdown_metadata(document)
-    md_splits = splitter.split_text(document)
-    
-    part = 1
-    for entry in md_splits:
-        entry.metadata = add_reference(entry.metadata) | basic_metadata
-        entry.metadata["Id"] = entry.metadata["No"] + "-" + str(part)
-        part += 1
-        
-    return md_splits
-
-
-def process_files(path, markdown_splitter):
-    document_splits = []  
-    
-    if path.is_dir():
-        md_files = sorted(path.rglob("*.md"))
-        for md_file in md_files:
-            print(md_file)
-            document_splits.extend(split_file(md_file, markdown_splitter))
-
-    elif path.is_file():
-        print(path)
-        document_splits.extend(split_file(path, markdown_splitter))
-
-    else:
-        raise FileNotFoundError(f"Path does not exist or is not a file/directory: {path}")
-    
-    
-    return document_splits
-    
-    # for doc in document_splits:
-    #     print(f"{doc.metadata["Reference"]} - {doc.metadata["No"]} - {doc.metadata["Id"]}")
-    # print(f"Total splits: {len(document_splits)}")
-    # ids = [doc.metadata["Id"] for doc in document_splits]
-    # pprint.pp(ids)
-
-
 def store_splits(splits):
-    """Store the document splits in ChromaDB"""
+    """ Store the document splits in ChromaDB """
     ids = [doc.metadata["Id"] for doc in splits]
     docs = [doc.page_content for doc in splits]
     metadatas = [doc.metadata for doc in splits]
-    # pprint.pp(ids)
-    # pprint.pp(docs)
-    # pprint.pp(metadata)
+
     telemetry_off = Settings(anonymized_telemetry=False)
     client = chromadb.PersistentClient(path="PythonFridayRAG.chroma", settings=telemetry_off)
 
